29bf759869f4c2e841fbf590a226fc63c3491022
Rajan Singh <rajansingh8593@gmail.com>
2025-03-27 10:17:55 +0530

    Update bookings.js

--- routes/bookings.js
+++ routes/bookings.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { sendBookingConfirmation } = require('../util/emailService');
 
-// Create a new booking
+
 router.post('/api/bookings', async (req, res) => {
     try {
         const bookingData = req.body;
